edf3f503d5feeff3af875553dff97a5ed00d9a4e
Alexc73 <xzibit_2007@yahoo.com>
2025-01-19 22:53:44 +0000

    Added running

--- Scripts/character.gd
+++ Scripts/character.gd
@@ -4,9 +4,11 @@ extends CharacterBody3D
 const JUMP_VELOCITY = 4.5
 
 @export var _walking_speed : float = 1
+@export var _running_speed : float = 2
 @export var _acceleration : float = 2
 @export var _deceleration : float = 4
 @export var _rotation_speed : float = PI * 2
+var _movement_speed : float = _walking_speed
 var _angle_difference : float
 var _xz_velocity : Vector3
 
@@ -18,12 +20,18 @@ var _direction : Vector3
 func move(direction : Vector3):
 	_direction = direction
 
+func walk():
+	_movement_speed = _walking_speed
+
+func run():
+	_movement_speed = _running_speed
+
 func _physics_process(delta: float) -> void:
-#		If the Player is giving movement input, face that direction
+	#If the Player is giving movement input, face that direction
 	if _direction:
 		_angle_difference = wrapf(atan2(_direction.x, _direction.z) - _rig.rotation.y, -PI, PI)
 		_rig.rotation.y += clamp(_rotation_speed * delta, 0, abs(_angle_difference)) * sign(_angle_difference)
-#	Copy the character's x and z velocity to isolate from y.
+	#Copy the character's x and z velocity to isolate from y.
 	_xz_velocity = Vector3(velocity.x, 0, velocity.z)
 	# Add the gravity.
 	if not is_on_floor():
@@ -38,13 +46,19 @@ func _physics_process(delta: float) -> void:
 	#var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	#var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
-#	Apply movement input to the xz velocity
+	#Apply movement input to the xz velocity
 	if _direction:
-		_xz_velocity = _xz_velocity.move_toward(_direction * _walking_speed, _acceleration * delta)
+		#Accelerate
+		if _direction.dot(velocity) >= 0:
+			_xz_velocity = _xz_velocity.move_toward(_direction * _movement_speed, _acceleration * delta)
+		#Turn around
+		else:
+			_xz_velocity = _xz_velocity.move_toward(_direction * _movement_speed, _deceleration * delta)
+		#Decelerate
 	else:
 		_xz_velocity = _xz_velocity.move_toward(Vector3.ZERO, _deceleration * delta)
 	
-	_animation.set("parameters/Locomotion/blend_position", _xz_velocity.length() / _walking_speed)
+	_animation.set("parameters/Locomotion/blend_position", _xz_velocity.length() / _running_speed)
 	
 #	Apply adjusted xz velocity to the character
 	velocity.x = _xz_velocity.x
